9f8f76632ca3d9d8e0ed436c745e26e864e183d9
optovolokn0 <dimathesokolov@gmail.com>
2022-05-23 19:43:49 +0500

    Tried adding db for processors

--- app/src/main/java/com/school/electronics/ui/all/Processors.java
+++ app/src/main/java/com/school/electronics/ui/all/Processors.java
@@ -1,19 +1,71 @@
 package com.school.electronics.ui.all;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.school.electronics.R;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Processors extends Fragment {
 
+    List<String> cpus = new ArrayList<String>();
+    int image = R.drawable.cpu;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_processors, container, false);
+        View view = inflater.inflate(R.layout.fragment_processors, container, false);
+
+
+        db.collection("products").document("electronics").collection("CPUs").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d("DOCUMENT", document.getId() + " => " + document.getData());
+                                cpus.add(document.getId());
+                                Log.d("DOCUMENT", cpus.toString());
+                            }
+                        } else {
+                            Log.d("DOCUMENT", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recView);
+        String[] allcpus = new String[cpus.size()];
+        cpus.toArray(allcpus);
+        MyAdapter myAdapter = new MyAdapter(this.getContext(), allcpus, image);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
     }
 }
